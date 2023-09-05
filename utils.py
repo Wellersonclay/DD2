99bc8efb14226000f033659477b0c153157a28b3
Wellersonclay <52115822+Wellersonclay@users.noreply.github.com>
2023-09-05 07:07:53 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,6 +13,19 @@ def predict(model, images, minDepth=10, maxDepth=1000, batch_size=2):
     # Put in expected range
     return np.clip(DepthNorm(predictions, maxDepth=maxDepth), minDepth, maxDepth) / maxDepth
 
+#Inserido
+
+def imagempadrao(images, minDepth=10, maxDepth=1000, batch_size=2):
+    # Support multiple RGBs, one RGB image, even grayscale 
+    if len(images.shape) < 3: images = np.stack((images,images,images), axis=2)
+    if len(images.shape) < 4: images = images.reshape((1, images.shape[0], images.shape[1], images.shape[2]))
+    # Compute predictions
+    predictions2 = images
+    # Put in expected range
+    return np.clip(predictions2, minDepth, maxDepth) / maxDepth
+
+#Inserido
+
 def scale_up(scale, images):
     from skimage.transform import resize
     scaled = []
