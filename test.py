96d0beba854caba52b0fffa601d7c3374ec9d7e8
Wellersonclay <52115822+Wellersonclay@users.noreply.github.com>
2023-09-07 17:59:58 -0300

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '5'
 from keras.models import load_model
 from layers import BilinearUpSampling2D
 from tensorflow.keras.layers import Layer, InputSpec
-from utils import predict, load_images, display_images, imagempadrao
+from utils import predict, load_images, display_images#, imagempadrao
 from matplotlib import pyplot as plt
 
 # Argument Parser
