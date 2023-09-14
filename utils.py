04379307f8d89849108308cf7ca4f157e3d3a8f2
Wellersonclay <52115822+Wellersonclay@users.noreply.github.com>
2023-09-14 01:00:06 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -11,8 +11,9 @@ def predict(model, images, minDepth=10, maxDepth=1000, batch_size=2):
     # Compute predictions
     predictions = model.predict(images, batch_size=batch_size)
     # Put in expected range
-    return np.clip(DepthNorm(predictions, maxDepth=maxDepth), minDepth, maxDepth) / maxDepth
-
+    #return np.clip(DepthNorm(predictions, maxDepth=maxDepth), minDepth, maxDepth) / maxDepth
+    return predictions
+    
 def scale_up(scale, images):
     from skimage.transform import resize
     scaled = []
